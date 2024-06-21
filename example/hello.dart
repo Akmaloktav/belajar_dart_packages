211282ed4f54c382d311c597d46e4b76c35c65d0
Akmaloktav <akmaloktavian139@gmail.com>
2024-06-21 22:08:21 +0700

    dart library

---
+++ example/hello.dart
@@ -0,0 +1,9 @@
+import 'package:belajar_dart_packages/hello.dart';
+
+void main(List<String> args) {
+  print(sayHello("akmal"));
+
+  var costumer = Costumer();
+  var product = Product();
+  var category = Category();
+}
